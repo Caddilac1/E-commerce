90c9efba21ff71ef99a92240a1aff45c96a1b6bf
Caddilac1 <155891049+Caddilac1@users.noreply.github.com>
2024-10-09 13:32:18 -0700

    added this file

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,16 @@
 console.log("Sammykeys Official is in");
+
+//getting all buttons
+
+var updatebuttons = document.getElementsByClassName('update-cart');
+console.log(updatebuttons);
+
+for(var i = 0; i < updatebuttons; i++){
+updatebuttons[i].addEventListener('click',function(){
+    var productId = this.dataset.product;
+    var action = this.dataset.action;
+    console.log("productId",productId);
+    console.log("Action",action);
+})
+}
+
