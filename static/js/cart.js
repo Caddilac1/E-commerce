44ec3659bb5d2b952619204dd3835b6d7e772073
agyesam12 <agyesam12@gmail.com>
2024-10-09 19:12:37 +0000

    added anoynmous user attribute to check for registered and non registered customer add to cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,16 +1,27 @@
-console.log("Sammykeys Official is in");
 
 //getting all buttons
+var updateBtns = document.getElementsByClassName('update-cart');
+console.log("updateBtns",updateBtns);
 
-var updatebuttons = document.getElementsByClassName('update-cart');
-console.log(updatebuttons);
+for(let i=0; i< updateBtns.length; i++){
+    console.log("Button:", updateBtns[i]);
 
-for(var i = 0; i < updatebuttons; i++){
-updatebuttons[i].addEventListener('click',function(){
-    var productId = this.dataset.product;
-    var action = this.dataset.action;
-    console.log("productId",productId);
-    console.log("Action",action);
-})
+    updateBtns[i].addEventListener('click',function(){
+        alert("button clicked");
+        console.log("Button clicked!");
+        console.log("Button:", this);
+
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log("ProductId",productId);
+        console.log("Action",action);
+
+    if(user == 'AnonymousUser'){
+        console.log("Not logged in")
+    }
+    else{
+        console.log("User is logged in sending data...");
+    }
+    })
 }
 
