80fdba52e8e7c2c0622b3ae37ffabe1a1794b388
agyesam12 <agyesam12@gmail.com>
2024-10-10 12:10:51 +0000

    empty template

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,27 +1,4 @@
 
-//getting all buttons
-var updateBtns = document.getElementsByClassName('update-cart');
-console.log("updateBtns",updateBtns);
 
-for(let i=0; i< updateBtns.length; i++){
-    console.log("Button:", updateBtns[i]);
 
-    updateBtns[i].addEventListener('click',function(){
-        alert("button clicked");
-        console.log("Button clicked!");
-        console.log("Button:", this);
-
-        var productId = this.dataset.product;
-        var action = this.dataset.action;
-        console.log("ProductId",productId);
-        console.log("Action",action);
-
-    if(user == 'AnonymousUser'){
-        console.log("Not logged in")
-    }
-    else{
-        console.log("User is logged in sending data...");
-    }
-    })
-}
 
