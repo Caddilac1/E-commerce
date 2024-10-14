a86e889fc4f8a91659b38d42e8024ad0abec32d1
agyesam12 <agyesam12@gmail.com>
2024-10-14 23:18:20 +0000

    updated the models to return readable messages instead of objects

--- shop/models.py
+++ shop/models.py
@@ -28,42 +28,45 @@ class Product(models.Model):
               url = ''
          return url
     
-class Order(models.Model):
-        Customer = models.ForeignKey(Customer ,on_delete=models.SET_NULL,null=True,blank=True)
-        date_ordered = models.DateTimeField(auto_now_add=True)
-        complete = models.BooleanField(default=False)
+class Order(models.Model):     
+     customer = models.ForeignKey(Customer ,on_delete=models.SET_NULL,null=True,blank=True)
+     date_ordered = models.DateTimeField(auto_now_add=True)
+     complete = models.BooleanField(default=False)
 
-        @property
-        def get_carttotal(self):
-         orderitem = self.orderitem_set.all()
-         total = sum([item.gettotal for item in orderitem])
-         return total
+     @property
+     def get_carttotal(self):
+          orderitem = self.orderitem_set.all()
+          total = sum([item.gettotal for item in orderitem])
+          return total
 
 
-        def _str_(self):
-            return str(self.id)
+     def __str__(self):
+            return self.customer.name
         
 
 class OrderItem(models.Model):
-    Product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL,null=True)
-    quantity = models.IntegerField(default=0,null=True,blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+     product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
+     order = models.ForeignKey(Order, on_delete=models.SET_NULL,null=True)
+     quantity = models.IntegerField(default=0,null=True,blank=True)
+     date_added = models.DateTimeField(auto_now_add=True)
 
-    @property
-    def gettotal(self):
-         total = self.quantity * self.Product.price
-         return total
+     @property
+     def gettotal(self):
+          total = self.quantity * self.product.price
+          return total
     
-    @property
-    def get_carttotal(self):
-         orderitem = self.Orderitem_set.all()
-         total = sum([item.gettotal for item in orderitem])
-         return total
+     @property
+     def get_carttotal(self):
+          orderitem = self.Orderitem_set.all()
+          total = sum([item.gettotal for item in orderitem])
+          return total
+
+     def __str__(self):
+          return self.product.name
 
 
 class ShippingAddress(models.Model):
-     Customer =models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+     customer =models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
      order = models.ForeignKey(Order, null=True,blank=True,on_delete=models.SET_NULL)
      address = models.CharField(max_length=220,null=True)
      city = models.CharField(max_length=220,null=True)
